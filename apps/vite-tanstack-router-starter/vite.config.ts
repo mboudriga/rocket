69cccc44490a979488dc6978cc301cd88c73b704
mbm-theman <theman@mbm.industries>
2026-04-10 14:54:37 -0400

    fix: spa starter vite config

--- apps/vite-tanstack-router-starter/vite.config.ts
+++ apps/vite-tanstack-router-starter/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: { port: 3000 },
+  resolve: { tsconfigPaths: true },
   plugins: [
     tanstackStart(), // MUST come before react()
     react(),
